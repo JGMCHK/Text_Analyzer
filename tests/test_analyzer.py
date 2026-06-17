21212b2c42b3b5fbbe223ce0351922cdf326e311
Javier García Martínez <jgm.roboind@hotmail.com>
2026-06-16 21:05:18 -0600

    Se crea repositorio y se suben los archivos que se han generado durante la clase

---
+++ tests/test_analyzer.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+#Agregar la ruta del paquete 
+sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Analyzer.text_analyzer import(
+    count_words,
+    count_characters,
+    count_sentences,
+    longest_word
+)
+
+def test_count_words():
+    text = "Hola mundo"
+    assert count_words(text) == 2
+    
+
+def test_count_characters():
+    text = "Hola"
+    assert count_characters(text) == 4
+
+def test_count_sentences():
+    text = "Hola mundo. Esto es una prueba."
+    assert count_sentences(text) == 2
+
+def test_longest_word():
+    text = "Hola mundo de programacion"
+    assert longest_word(text)
+
+if __name__ == '__main__':
+    print('Ejecutando tests...')
+    test_count_characters()
+    test_count_sentences()
+    test_count_words()
+    test_longest_word()
+    print('Todos los test fueron ejecutados de manera correcta')
